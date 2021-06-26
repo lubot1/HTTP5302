503d15a20f6d1f7d5e1195ee169298dc204b5ab3
Luis Botello <botello48@gmailcom>
2021-06-25 20:28:41 -0400

    Added components in

---
+++ VueGame/Game/src/main.js
@@ -0,0 +1,36 @@
+Vue.component('square', { //Lab 6 vue componenets
+    props: ['x', 'y', 'player'],
+    template: 
+    `<div
+        v-bind:class="'gameSquare'"
+        v-bind:style="{'height':'100px', 'width':'100px', 'border':'1px black solid'}">
+    </div>`
+});
+
+var title = new Vue({
+    el: '#title',
+    data: {
+        style: { //Lab 1 Binding html attributes (style)
+            color: "red",
+            fontSize: "1em",
+        },
+        title: 'Connect 4', //Lab 0 prototype step
+    }
+});
+var app = new Vue({
+    el: '#app',
+    data: {
+        squares: [],
+        playerTurn: true, //Lab 3 conditionals
+    }
+});
+// I'm too lazy to write out 42 square properties so I just push them below into an array
+var counter = 0;
+var i = 0;
+var j = 0;
+for (let i = 0; i < 7; i++) {
+    for (let j = 0; j < 6; j++) {
+        app.$data.squares.push({id: counter, x: i, y: j, player: null});
+        counter++;
+    }
+}
